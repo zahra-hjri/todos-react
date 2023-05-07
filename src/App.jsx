33f90282281e7af029efa42c497117247103f4c3
Zahra Hojabri <zhojabri@ymail.com>
2023-05-07 07:53:04 +0430

    create new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <h1 className='text-3xl font-bold underline btn btn-success'>Vite + React</h1>
+
+    </>
+  )
+}
+
+export default App
